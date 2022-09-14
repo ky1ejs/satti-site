db97645c240c383477d264b5ca3b8c57ba7d7cf8
Kyle Satti <k@kylejs.dev>
2022-09-14 12:01:26 +0100

    remove css import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { Center } from '../components/Center'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => (
   <div className={styles.container}>
