150ad88b5e9746e2dd64857cc62e0eb7ace86b2e
Kyle Satti <k@kylejs.dev>
2022-09-14 12:00:28 +0100

    update index

---
+++ components/Center.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from "react";
+import css from "./Center.module.css"
+
+export function Center({children}: {children: ReactElement}) {
+  return (
+    <div className={css.container}>
+      <div className={css.content}>{children}</div>
+    </div>
+  );
+};
